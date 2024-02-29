093a37327a57140f4c480dbcdab8df68c996fab3
jakub5016 <me.jakub.gil@gmail.com>
2024-03-01 00:22:58 +0100

    Init commit

---
+++ src/main/java/com/hurtowania/hurtowniaspozywcza/HurtowniaSpozywczaApplication.java
@@ -0,0 +1,13 @@
+package com.hurtowania.hurtowniaspozywcza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurtowniaSpozywczaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurtowniaSpozywczaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hurtowania/hurtowniaspozywcza/HurtowniaSpozywczaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurtowania.hurtowniaspozywcza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurtowniaSpozywczaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
